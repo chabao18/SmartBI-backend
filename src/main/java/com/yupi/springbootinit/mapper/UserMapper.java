6f7ac86f14d3f4cfe45d1abd238089aafee8f2ed
zxlu <zhixiong-lu@qq.com>
2024-07-11 15:32:42 +0800

    初始测试

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author tbag
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-07-11 15:06:55
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author tbag
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2024-07-11 15:06:55
+*/
+public interface ChartService extends IService<Chart> {
+
+}
